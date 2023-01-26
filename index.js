5f02c907172a841bcbc383410d6199fcff0d4667
Aaron Hope <aghopecode@gmail.com>
2023-01-25 17:10:26 -0800

    Added transparent shade to nav menu

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const navbar = document.querySelector('nav');
+const navBackground = document.getElementById('nav-background');
 const openMenu = document.getElementById('menu-button');
 const closeMenu = document.getElementById('menu-close');
 
 openMenu.addEventListener('click', () => {
   navbar.classList.add('open');
   navbar.classList.remove('closed');
+  navBackground.classList.remove('closed');
 });
 
 closeMenu.addEventListener('click', () => {
   navbar.classList.remove('open');
   navbar.classList.add('closed');
+  navBackground.classList.add('closed');
 });
